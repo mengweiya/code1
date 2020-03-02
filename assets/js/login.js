5cb2482094ff7cf53d40e713ffbae874691af36f
unknown <451260862@qq.com>
2020-03-02 16:57:51 +0800

    完成注册和登陆页面

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,9 @@
 $(function () {
+
+    // 注册行为:
+
     var form = layui.form
+    var layer = layui.layer
     // 点击了注册的链接
     $('#link-reg').on('click', function () {
         $('.login-box').hide() // 隐藏
@@ -27,6 +31,48 @@ $(function () {
                 return '两次密码不一致'
             }
         }
+    })
+
+
+    // 监听注册表单的提交事件
+    $('#form-reg').on('submit', function (e) {
+        // 阻止表单默认提交
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: 'http://www.liulongbin.top:3007/api/reguser',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 1) {
+                    return layer.msg(res.message)
+                }
+                // layer.msg(res.message);  //注册成功
+                layer.msg('注册成功!请登陆~');
+            }
+        })
+
+    })
+    // 监听登录表单的提交事件
+    $('#form-login').on('submit', function (e) {
+        // 阻止表单默认提交
+        e.preventDefault();
+        $.ajax({
+            type: 'post',
+            url: 'http://www.liulongbin.top:3007/api/login',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                // 提示用户登录成功
+                layer.msg('登录成功！')
+                // 将服务器颁发的 token 字符串，持久化存储到 localStorage
+                localStorage.setItem('token', res.token)
+                // 跳转到后台首页
+                location.href = '/index.html'
+            }
+        })
+
     })
 
 
